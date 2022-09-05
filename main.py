53d6a2f11e4ff0ba66f42ec2086ca245d844468f
Totally Real Penguin <johnthomson190921@gmail.com>
2022-09-05 19:36:46 +0100

    added bug-report command

--- main.py
+++ main.py
@@ -15,37 +15,45 @@ bot = Bot(guild_ids=GUILDS, intents=intents)
 async def on_ready():
     print(f'{bot.user.display_name} Is Alive')
 
-@bot.slash_command(name='suggestion',description='Suggest a new command')
+@bot.slash_command(name='suggestion',description='Suggest a new command', guild_ids=GUILDS)
 async def command(ctx, suggestion: Option(str,'Suggestion Command',required=True)):
     try:
         await ctx.respond(f'Suggestion Noted')
-        await bot.get_channel(LOG_CHANNEL).send(f'```{ctx.author.display_name} Suggested: \n{suggestion}```')
+        await bot.get_channel(SUGGESTION_CHANNEL).send(f'```{ctx.author.display_name} Suggested: \n{suggestion}```')
     except:
-        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In Bug-Reports', ephemeral=True)
+        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In /bug_report', ephemeral=True)
 
-@bot.slash_command(name='ty', description='Just Says Thank You')
+@bot.slash_command(name='bug_report',description='report a bug', guild_ids=GUILDS)
+async def command(ctx, bug: Option(str,'Bug Description',required=True)):
+    try:
+        await ctx.respond(f'Bug Noted')
+        await bot.get_channel(SUGGESTION_CHANNEL).send(f'```{ctx.author.display_name} Suggested: \n{bug}```')
+    except:
+        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In /bug_report', ephemeral=True)
+
+@bot.slash_command(name='ty', description='Just Says Thank You', guild_ids=GUILDS)
 async def command(ctx):
     try:
         await ctx.respond(f'Thank You {ctx.author.display_name}', ephemeral=True)
     except:
-        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In Bug-Reports', ephemeral=True)
+        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In /bug_report', ephemeral=True)
 
-@bot.slash_command(name='connect', description='Connect Bot To A Voice Channel')
+@bot.slash_command(name='connect', description='Connect Bot To A Voice Channel', guild_ids=GUILDS)
 async def command(ctx):
     try:
         channel = ctx.author.voice.channel
         await channel.connect()
         await ctx.respond(f'Connected To {channel}')
     except:
-        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In Bug-Reports', ephemeral=True)
+        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In /bug_report', ephemeral=True)
 
-@bot.slash_command(name='disconnect', description='Disconnects From A Voice Channel')
+@bot.slash_command(name='disconnect', description='Disconnects From A Voice Channel', guild_ids=GUILDS)
 async def command(ctx):
     try:
         await ctx.respond(f'Disconnected')
         await ctx.voice_client.disconnect()
     except:
-        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In Bug-Reports', ephemeral=True)    
+        await ctx.respond(f'{ctx.author.display_name} An Error Occured Try Again If It Keeps Happening Report In /bug_report', ephemeral=True)    
 
 try:
     bot.run(TOKEN)
